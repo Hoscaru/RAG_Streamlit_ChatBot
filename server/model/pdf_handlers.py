3fb22913567e5748f8fcd0425817f6a962005462
Hoscar <oscarodriguez.05.18@gmail.com>
2025-06-14 10:17:50 -0400

    padfs handler added

---
+++ server/model/pdf_handlers.py
@@ -0,0 +1,26 @@
+import os
+import shutil
+from fastapi import UploadFile
+import tempfile
+
+UPLOAD_DIR = "./uploaded_pdfs"
+
+def save_uploaded_files(files:;ost[UploadFile]) -> list[str]:
+    """
+    Save uploaded PDF files to a temporary directory and return their paths.
+    
+    Args:
+        files (list[UploadFile]): List of uploaded files.
+    
+    Returns:
+        list[str]: List of file paths where the PDFs are saved.
+    """
+    os.makedirs(UPLOAD_DIR, exist_ok=True)
+    file_paths = []
+
+    for file in files:
+        temp_path = os.path.join(UPLOAD_DIR, file.filename)
+        with open(temp_path, "wb") as f:
+            shutil.copyfileobj(file.file, f)
+        file_paths.append(temp_path)
+    return file_paths
